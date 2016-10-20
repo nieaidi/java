7b46d4eb8d223314e9ee68c67adb3c8414e98a4c
nad <1228797740@qq.com>
2016-10-20 20:36:22 +0800

    编写一个名称为Person类，定义三个变量分别表示人的姓名，学号，性别，提供构造方法改变成员变量，此外还有一个不能被其他类继承的run方法。

---
+++ src/javaTest/Work1.java
@@ -0,0 +1,50 @@
+package javaTest;
+
+public class Work1 {
+	  public static void main(String grgs[])
+	  {
+		  Person p=new Person("小明","男",16);
+		  System.out.println(p.getName() +"的性别是"+p.getSex() +
+				  ",年龄是"+p.getAge() );
+		  p.run1() ;
+		 
+	  }
+	}
+	class Person
+	{ 
+		private String name;
+		private String sex;
+		private int age;
+		public  Person(String name,String sex,int age){
+			this.name =name;
+			this.sex =sex;
+			this.age =age;
+		}
+		 private void run(){
+			 System.out.println("人类会跑");
+			
+		}
+		 public void run1(){
+			 run();
+		 }
+		public int getAge() {
+			return age;
+		}
+		public void setAge(int age) {
+			this.age = age;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getSex() {
+			return sex;
+		}
+		public void setSex(String sex) {
+			this.sex = sex;
+		}
+		 
+		
+	}
